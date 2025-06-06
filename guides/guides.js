189db5e4790dc81b2c4089d1be263792575906de
BloxerHD <bloxerhd018+github@gmail.com>
2025-06-06 22:49:48 +0100

    Replaced Placeholder Installation Guides with Full Guides

--- guides/guides.js
+++ guides/guides.js
@@ -1,4 +1,5 @@
 const guideContents = document.querySelectorAll(".guide-content");
+const guideButtons = document.querySelectorAll(".guide");
 
 function changeGuide(newID) {
     guideContents.forEach(content => {
@@ -6,6 +7,17 @@ function changeGuide(newID) {
             content.style.display = "block";
         } else {
             content.style.display = "none";
+        }
+    })
+
+    guideButtons.forEach(button => {
+        let gid = button.id.slice(1);
+        if (gid == newID) {
+            button.classList.add("active");
+        } else {
+            if (button.classList.contains("active")) {
+                button.classList.remove("active");
+            }
         }
     })
 }
